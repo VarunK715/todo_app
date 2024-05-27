a1c6d962c28a24c675d39cbe6588c9640b072993
varunk <varunakumar735@gmail.com>
2024-05-27 17:01:06 +0530

    moved pagination code outside

--- djangoproject/myfirsttodoapp/views.py
+++ djangoproject/myfirsttodoapp/views.py
@@ -13,6 +13,7 @@ def home(request):
     return render(request,'todoapp/home.html')
 
 @login_required
+
 def task(request):
     if request.method == 'POST':
         if 'task' in request.POST:
@@ -35,17 +36,17 @@ def task(request):
             dataf = ToDoApp.objects.filter(user=request.user)
     else:
         dataf = ToDoApp.objects.filter(user=request.user)
-        #print(f"inside pagination --- {dataf}")
-        paginator = Paginator(dataf, 4)  # Show 2 tasks per page
-        page_number = request.GET.get('page')
-        try:
-            page_obj = paginator.get_page(page_number)  # returns the desired page object
-        except PageNotAnInteger:
-            # if page_number is not an integer then assign the first page
-            page_obj = paginator.page(1)
-        except EmptyPage:
-            # if page is empty then return last page
-            page_obj = paginator.page(paginator.num_pages)
+
+    paginator = Paginator(dataf,3)  # Show 4 tasks per page
+    page_number = request.GET.get('page')
+    try:
+        page_obj = paginator.get_page(page_number)  # returns the desired page object
+    except PageNotAnInteger:
+        # if page_number is not an integer then assign the first page
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        # if page is empty then return last page
+        page_obj = paginator.page(paginator.num_pages)
 
     total = len(dataf)
     tomorrow = date.today() + timedelta(days=1)
@@ -59,8 +60,7 @@ def task(request):
     completed_task = ToDoApp.objects.filter(user=request.user, is_task_completed=True)
     completed_total = len(completed_task)
     
-    return render(request, 'todoapp/index.html', {'total_count': total, 'completed': completed_total,"page_obj": page_obj})
-
+    return render(request, 'todoapp/index.html', {'dataf': dataf, 'total_count': total, 'completed': completed_total, "page_obj": page_obj})
 
 @login_required
 def update_task(request,task_id):
